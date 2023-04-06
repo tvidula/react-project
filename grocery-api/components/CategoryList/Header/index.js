0aaae463fabbba408c78341d23b292388dcd352f
tvidula <40817337+tvidula@users.noreply.github.com>
2023-04-06 14:03:45 +0530

    Create index.js

---
+++ grocery-api/components/CategoryList/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="jumbotron text-center">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">
+       Welcome to Grocery App your one stop shop for all your grocery need
+      </p>
+      <hr class="my-4" />
+      <p>
+        It uses utility classes for typography and spacing to space content out
+        within the larger container.
+      </p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">
+        Start Shoping
+      </a>
+    </div>
+  );
+};
+export default Header;
